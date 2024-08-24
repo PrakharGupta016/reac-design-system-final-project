cedc264fa5fa198902934135d7c355ce9d755f3a
PrakharGupta016 <86834271+PrakharGupta016@users.noreply.github.com>
2024-08-25 00:36:33 +0530

    content container changes

--- src/App.js
+++ src/App.js
@@ -5,32 +5,39 @@ import { Center } from "./components/8-center-pattern/start";
 import { Split } from "./components/2-split-pattern/start";
 import { SideBar } from "./components/final-project/SideBar";
 import styled from "styled-components";
-import './index.css'
+import "./index.css";
 import { Form } from "./components/final-project/form";
 import { InlineBundle } from "./components/5-inline-bundle-pattern/start";
-const DetailsContainer = styled.div`
-    background:white;
-    color:black;
-    border-radius:8px;
 
-`
-
+const ContentContainer = styled(Pad).attrs(() => ({
+  padding: "xl",
+}))`
+  background-image: linear-gradient(
+    to bottom,
+    #0f1623 14rem,
+    rgb(242, 242, 242) 14rem
+  );
+`;
+const StyledSplit = styled(Split).attrs(() => ({
+  gutter: "none",
+  fraction: "1/4",
+}))`
+  background: white;
+  border-radius: 0.5rem;
+`;
 const App = () => {
   return (
     <>
-        <Layers>
-          <NavBar />
-          <Pad padding={["none","xl"]}>
-            <h1>Account Settings</h1>
-          
-            <DetailsContainer as={Pad} padding="l">
-              <Split fraction="1/3">
-                <SideBar/>
-                <Form/>
-              </Split>
-            </DetailsContainer>
-            </Pad>
-        </Layers>
+      <NavBar />
+      <ContentContainer>
+        <Center as={Layers} maxWidth="70rem" gutter="xl">
+          <h1 style={{ color: "white" }}>Account Settings</h1>
+          <StyledSplit>
+            <SideBar />
+            <Form />
+          </StyledSplit>
+        </Center>
+      </ContentContainer>
     </>
   );
 };
--- src/components/final-project/SideBar.jsx
+++ src/components/final-project/SideBar.jsx
@@ -1,16 +1,28 @@
 
+import styled from "styled-components"
 import { Layers } from "../1-layers-pattern/start"
 
-export const SideBar = ()=>{
-    return(<>
-        <Layers>
-            <span>profile</span>
-            <span>dc</span>
-            <span>cds</span>
-            <span>sdc</span>
-            <span>cds</span>
-        </Layers>
-    
-    
+const SideMenuContainer = styled(Layers).attrs(() => ({
+    as: "ul",
+    gutter: "xs",
+}))`
+    list-style-none:none;
+    padding-left:0;
+`;
+
+export const SideBar = () => {
+    return (<>
+        <nav>
+            <SideMenuContainer>
+                <li>Profile</li>
+                <li>Settings</li>
+                <li>Authentication</li>
+                <li>Email</li>
+                <li>Plans</li>
+                <li>APIS</li>
+            </SideMenuContainer>
+        </nav >
+
+
     </>)
 }
