7883637b7e2d33a64f238ff8a17a68487cfa40d5
PrakharGupta016 <86834271+PrakharGupta016@users.noreply.github.com>
2024-08-25 01:40:15 +0530

    app completed

--- src/components/final-project/form.jsx
+++ src/components/final-project/form.jsx
@@ -2,76 +2,94 @@ import styled from "styled-components"
 import { Layers } from "../1-layers-pattern/start"
 import { Column, Columns } from "../3-columns-pattern/start"
 import { InlineBundle } from "../5-inline-bundle-pattern/start";
+import { Pad } from "../7-pad-pattern/start";
+import { Split } from "../2-split-pattern/start";
+import { Logo } from "./Logo";
 
 export const customLabel = styled.input`
     font-size:10px;
     color:blue;
 `;
+const FormContainer = styled.div`
+    border-inline-start: 1px solid lightgray;
+    > * + * {
+        border-block-start: 1px solid lightgray;
+    }
+`;
+const CustomButton = styled(Pad).attrs(() => ({ as: "button", padding: ['m', 'l'] }))`
+    border: ${({ primary }) => primary ? "1px solid transparent" : "1px solid #0f1623"};
+    color:${({ primary }) => primary ? "white" : "#0f1623"};
+    background:${({ primary }) => primary ? "#0f1623" : "white"};
+    border-radius:0.25rem;
+        `;
+const CustomInput = ({ label, children }) => {
+    return (<Layers gutter='s' as='label'>
+        {label}
+        {children}
+    </Layers>)
+}
+
+const StyledInput = styled.input`
+    border: 1px solide lightgrey;
+broder - radius: 0.25px;
+color: #3030303;
+height: 30px;
+
+`;
 export const Form = () => {
-    return (<>
-
-        <form>
-            <Layers gutter='md'>
-            <span>
-                knn
-            </span>
-
-            <Columns columns='6' gutter='s'>
-                <Column $span='4'>
-                    <Layers>
-                        <label >dc</label>
-                        <input id='name' placeholder="name" />
-                    </Layers>
-                </Column>
-
-
-        
-                <Column $span='2'>
-                    <Layers>
-                        <label htmlFor="name">Image</label>
-                        <input id='name' placeholder="name" />
-                    </Layers>
-                </Column>
-
-
-                    <Column $span='3'>
+    return (<FormContainer>
+        <Pad as="section" padding='l'>
+            <Layers >
+                <Layers as="header">
+                    <h2>General Information</h2>
+                    <span> basic verification info</span>
+                </Layers>
+                <div>
+                    <Split fraction='auto-end'>
                         <Layers>
-                            <label htmlFor="name">name</label>
-                            <input id='name' placeholder="name" />
+                            <CustomInput label={'Username'}>
+                                <StyledInput />
+                            </CustomInput>
+                            <CustomInput label='About'>
+                                <textarea />
+                                <span> Tell a brief </span>
+                            </CustomInput>
+
                         </Layers>
-                    </Column>
+                        <CustomInput label={'Image'}>
+                            <Logo size='4rem' inverse circle />
+                        </CustomInput>
+                    </Split>
+                </div>
+                <Columns columns={2} gutter="l">
+                    <CustomInput label='First Name'>
+                        <StyledInput />
+                    </CustomInput>
+
+                    <CustomInput label='Last Name'>
+                        <StyledInput />
+                    </CustomInput>
+
+                    <CustomInput label='Linkedin'>
+                        <StyledInput />
+                    </CustomInput>
+
+                    <CustomInput label='Employer'>
+                        <StyledInput />
+                    </CustomInput>
+
+                </Columns>
 
 
-                    <Column $span='3'>
-                        <Layers>
-                            <label htmlFor="name">Image</label>
-                            <input id='name' placeholder="name" />
-                        </Layers>
-                    </Column>
 
 
-                    <Column $span='3'>
-                        <Layers>
-                            <label htmlFor="name">name</label>
-                            <input id='name' placeholder="name" />
-                        </Layers>
-                    </Column>
-
-
-                    <Column $span='3'>
-                        <Layers>
-                            <label htmlFor="name">Image</label>
-                            <input id='name' placeholder="name" />
-                        </Layers>
-                    </Column>
-            </Columns>
-            <InlineBundle justify='end'>
-                    <button>knv</button>
-                    <button>nj</button>
-                </InlineBundle>
             </Layers>
 
+        </Pad>
 
-        </form>
-    </>)
+        <InlineBundle as={Pad} justify='end' padding='l'>
+            <CustomButton>Cancel</CustomButton>
+            <CustomButton primary>Save</CustomButton>
+        </InlineBundle>
+    </FormContainer>)
 }
